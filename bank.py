c525a891119d891a419267735ab3ac8a3a31ade6
Lagomorph <godzilka@gmail.com>
2022-01-13 13:48:12 -0800

    added method chaining

--- bank.py
+++ bank.py
@@ -12,38 +12,30 @@ class User:
     def make_deposit(self, amount):
         self.account_balance += amount
         print(f"Deposti successful. Account balance for {self.first_name} {self.last_name} account ID({self.account_id}) is now {self.account_balance}")
+        return self
 
     def make_withdrawl(self, amount):
         self.account_balance += -abs(amount)
         print(f"Withdrawl successful. Account balance for {self.first_name} {self.last_name} account ID({self.account_id}) is now {self.account_balance}")
+        return self
 
     def display_user_balance(self):
         print(f"The balance for account {self.account_id} is: {self.account_balance}")
+        return self
     
     def transfer_money(self, other_user, amount):
         other_user.make_deposit(amount)
         print(f"Funds of {amount} were transfered to {other_user.first_name} {other_user.last_name} from account {self.account_id}")
         self.make_withdrawl(amount)
+        return self
 
 
-nathan = User("Torgl", "Thorson", "tthorson@gmail.com")
-print(nathan.account_id)
-nathan.make_deposit(10000)
-nathan.make_deposit(100000)
-nathan.make_deposit(80000)
-nathan.make_withdrawl(13000)
-nathan.display_user_balance()
-ilana = User("Freya","Njörðrsdaughter", "hotFreya@gmail.com")
-ilana.make_deposit(56000)
-ilana.make_deposit(603995)
-ilana.make_withdrawl(13000)
-ilana.make_withdrawl(25000)
-ilana.display_user_balance()
-nathan.transfer_money(ilana, 3700)
+torgl = User("Torgl", "Thorson", "tthorson@gmail.com")
+print(torgl.account_id)
+torgl.make_deposit(10000).make_deposit(100000).make_deposit(80000).make_withdrawl(13000).display_user_balance()
+freya = User("Freya","Njörðrsdaughter", "hotFreya@gmail.com")
+freya.make_deposit(56000).make_deposit(603995).make_withdrawl(13000).make_withdrawl(25000).display_user_balance()
+torgl.transfer_money(freya, 3700)
 
 jdoe = User("John", "Doe", "jdoe@gmail.com")
-jdoe.make_deposit(5000)
-jdoe.make_withdrawl(1200)
-jdoe.make_withdrawl(749)
-jdoe.make_withdrawl(8.99)
-jdoe.account_balance
+jdoe.make_deposit(5000).make_withdrawl(1200).make_withdrawl(749).make_withdrawl(8.99).display_user_balance()
